95d07c75bd4be4dca8426acf007a851f7ea2e400
LordRonz <aarontanhar2000@gmail.com>
2021-11-14 08:27:35 +0700

    first commit

---
+++ 4/anim.js
@@ -0,0 +1,13 @@
+anime.timeline({loop: 1})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: function(el, i) {
+      return 800 * i;
+    }
+ })
+
+ 
---
+++ 4/side.js
@@ -0,0 +1,5 @@
+
+function trigger(){
+  document.getElementById("sidebar").classList.toggle('active');
+  document.getElementById("kontainer").classList.toggle('active');
+}
